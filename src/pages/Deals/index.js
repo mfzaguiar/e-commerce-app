47416e1258830225c99a2d0b2207e2a708f59d37
mfzaguiar <first_mf@hotmail.com>
2019-12-19 21:11:54 -0300

    feat: christmas lottie animation

--- src/pages/Deals/index.js
+++ src/pages/Deals/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Animated, FlatList } from 'react-native';
+import { FlatList } from 'react-native';
 import CountDown from 'react-native-countdown-component';
 
 import {
@@ -7,37 +7,25 @@ import {
   HeaderCountDown,
   ContainerGradient,
   WrapperCount,
-  HeaderImage,
   Title,
   SpinnerLoading,
+  Animation,
 } from './styles';
 
 import ProductItem from '~/components/ProductItem';
-import bfImage from '~/assets/images/black-friday.png';
-import api from '~/services/api';
+import christmas from '~/assets/animations/merrychristmas.json';
 
-const HEADER_MIN_HEIGHT = 50;
-const HEADER_MAX_HEIGHT = 200;
+import api from '~/services/api';
 
 export default function Deals({ navigation }) {
   const [loading, setLoading] = useState(true);
   const [products, setProduts] = useState([]);
-  const [scrollOffsetY] = useState(new Animated.Value(0));
-
-  const headerHeight = scrollOffsetY.interpolate({
-    inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
-    outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
-    extrapolate: 'clamp',
-  });
 
   useEffect(() => {
     async function loadProducts() {
       const response = await api.get('products');
-
       const discountProducts = response.data.filter(p => p.discount > 0);
-
       setProduts(discountProducts);
-      // setProduts(response.data);
       setLoading(false);
     }
     loadProducts();
@@ -45,60 +33,41 @@ export default function Deals({ navigation }) {
 
   return (
     <ContainerGradient>
-      <Animated.ScrollView
-        contentContainerStyle={{ paddingTop: HEADER_MAX_HEIGHT }}
-        scrollEventThrottle={16}
-        onScroll={Animated.event([
-          { nativeEvent: { contentOffset: { y: scrollOffsetY } } },
-        ])}
-      >
-        <Animated.View
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            height: headerHeight,
-          }}
-        >
-          <WrapperCount>
-            <HeaderImage source={bfImage} />
+      <WrapperCount>
+        <Animation autoPlay loop source={christmas} />
+        <HeaderCountDown>
+          <Title>Oferta de Natal por tempo limitado</Title>
+          <CountDown
+            until={60 * 10 + 30}
+            digitStyle={{ backgroundColor: 'rgba(255,255,255,0.8)' }}
+            digitTxtStyle={{ color: '#333333' }}
+            timeLabelStyle={{
+              color: '#fff',
+              fontSize: 10,
+              fontWeight: 'bold',
+            }}
+            separatorStyle={{ color: '#fff' }}
+            timeToShow={['H', 'M', 'S']}
+            showSeparator
+            size={20}
+          />
+        </HeaderCountDown>
+      </WrapperCount>
 
-            <HeaderCountDown>
-              <Title>Oferta por tempo limitado</Title>
-              <CountDown
-                until={60 * 10 + 30}
-                digitStyle={{ backgroundColor: '#f95428' }}
-                digitTxtStyle={{ color: '#fff' }}
-                timeLabelStyle={{
-                  color: '#fff',
-                  fontSize: 10,
-                  fontWeight: 'bold',
-                }}
-                separatorStyle={{ color: '#fff' }}
-                timeToShow={['H', 'M', 'S']}
-                showSeparator
-                size={20}
-              />
-            </HeaderCountDown>
-          </WrapperCount>
-        </Animated.View>
-
-        {loading ? (
-          <SpinnerLoading />
-        ) : (
-          <Container>
-            <FlatList
-              numColumns={1}
-              data={products}
-              keyExtractor={item => String(item.id)}
-              renderItem={({ item }) => (
-                <ProductItem item={item} navigation={navigation} />
-              )}
-            />
-          </Container>
-        )}
-      </Animated.ScrollView>
+      {loading ? (
+        <SpinnerLoading />
+      ) : (
+        <Container>
+          <FlatList
+            numColumns={1}
+            data={products}
+            keyExtractor={item => String(item.id)}
+            renderItem={({ item }) => (
+              <ProductItem item={item} navigation={navigation} />
+            )}
+          />
+        </Container>
+      )}
     </ContainerGradient>
   );
 }
--- src/pages/Deals/styles.js
+++ src/pages/Deals/styles.js
@@ -1,30 +1,34 @@
 import styled from 'styled-components/native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { ActivityIndicator, Dimensions } from 'react-native';
+import LottieView from 'lottie-react-native';
+
 import { Animated } from 'react-native';
 
-export const Container = styled.View`
-  padding: 0 5px;
-`;
-
 export const ContainerGradient = styled(LinearGradient).attrs({
-  colors: ['#ff6347', '#1c1919', '#ff6347'],
+  // colors: ['#ff6347', '#1c1919', '#ff6347'],
+  colors: ['#dd092c', '#1c1919', '#dd092c'],
 })`
   flex: 1;
 `;
 
+export const Container = styled.ScrollView`
+  padding: 0 5px;
+  margin-bottom: 5px;
+  height: ${Dimensions.get('window').height * 0.5};
+`;
+
 export const WrapperCount = styled.View`
-  flex-direction: row;
   align-items: center;
-  justify-content: space-evenly;
-  padding: 50px 0;
+  height: 200px;
 `;
 
 export const HeaderImage = styled.Image.attrs({
   resizeMode: 'contain',
 })`
+  flex: 1;
   height: ${Dimensions.get('window').width * 0.25};
-  width: ${Dimensions.get('window').width * 0.25};
+  width: ${Dimensions.get('window').width * 1};
 `;
 
 export const Title = styled(Animated.Text).attrs({})`
@@ -36,14 +40,26 @@ export const Title = styled(Animated.Text).attrs({})`
 `;
 
 export const SpinnerLoading = styled(ActivityIndicator).attrs({
-  color: 'tomato',
+  color: '#fff',
   alignSelf: 'center',
   marginTop: 30,
   size: 'large',
 })``;
 
 export const HeaderCountDown = styled.View`
+  position: absolute;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  background: rgba(0, 0, 0, 0.5);
+  margin-top: 50px;
+  padding: 10px;
+  border-radius: 4px;
+`;
+
+export const Animation = styled(LottieView).attrs({
+  resizeMode: 'cover',
+})`
+  width: ${Dimensions.get('window').width * 1};
+  max-height: ${Dimensions.get('window').width * 1};
 `;
